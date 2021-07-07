07170e5f8aecda59db1e082138668eb1fbe4b6b9
Corey Psoinos <coreypsoinos@gmail.com>
2021-07-07 09:06:34 -0400

    get words from random-word-api

--- src/logic/useWord.ts
+++ src/logic/useWord.ts
@@ -1,3 +1,13 @@
-export const useWord = () => {
-  return 'foo bar'
+
+export const useWord = async () => {
+  const url = 'https://random-word-api.herokuapp.com/word?number=10&swear=0'
+
+  try {
+    const resp = await fetch(url)
+    if (resp.ok) return await resp.json()
+    else throw (new Error('Error fetching random words!'))
+  }
+  catch (err) {
+    console.error(err)
+  }
 }
