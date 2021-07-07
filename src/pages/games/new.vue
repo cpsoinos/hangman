bc61af21cca2c5ae3b5673db7fb7df81da1d6fc3
Corey Psoinos <coreypsoinos@gmail.com>
2021-07-06 23:34:40 -0400

    rename page to new

---
+++ src/pages/games/new.vue
@@ -0,0 +1,13 @@
+<template>
+</template>
+
+<script setup lang="ts">
+import { useHead } from '@vueuse/head'
+
+useHead({
+  title: 'New Game',
+  meta: [
+    { name: 'description', content: 'Create a new game of Hangman' },
+  ],
+})
+</script>
