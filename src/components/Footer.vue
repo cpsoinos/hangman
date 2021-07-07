84a78ac8ca09ee4de928051a3be218beed564a9f
Corey Psoinos <coreypsoinos@gmail.com>
2021-07-06 21:28:37 -0400

    update url to gh repo

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -13,7 +13,7 @@
       <carbon-dicom-overlay />
     </router-link>
 
-    <a class="icon-btn mx-2" rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank" title="GitHub">
+    <a class="icon-btn mx-2" rel="noreferrer" href="https://github.com/cpsoinos/hangman" target="_blank" title="GitHub">
       <carbon-logo-github />
     </a>
   </nav>
